89c5506006528d782c4702e0a5e08a00b21b1817
mdjun438 <junaid17chk@gmail.com>
2024-11-03 00:04:49 +0500

    reset-password implemented

---
+++ Front-End/src/components/auth/reset-password.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+
+
+const ResetPassword = () => {
+
+  return (
+    <div>
+        <h1>Reset Password</h1>
+    </div>
+  );
+};
+
+export default ResetPassword;
