1474ff97909a6ca1793d7f3c98ec02263f6e2917
mdjun438 <137438181+mdjun438@users.noreply.github.com>
2024-11-02 23:30:07 +0500

    forget-password Functionality

---
+++ Front-End/src/components/auth/forget-password.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+
+
+const ForgetPassword = () => {
+
+  return (
+    <div>
+        <h1>ForgetPassword</h1>
+    </div>
+  );
+};
+
+export default ForgetPassword;
