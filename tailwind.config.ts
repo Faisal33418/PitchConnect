5f7da31b701e1b0432d402dc7bd4859874964af9
unknown <33418@students.riphah.edu.pk>
2025-04-07 00:24:30 +0500

    update tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layout/**/*.{js,jsx,ts,tsx,mdx}",
+    './node_modules/@syncfusion/**/*.css', // Ensure Syncfusion styles are not purged
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
