b22837869c7a22aa5837ccfc88f8f7f9df48690c
unknown <ahsanakmalbit99@gmail.com>
2025-04-21 20:10:44 +0500

    Messaging between users

---
+++ src/components/messages/MyMessages.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MessagesPopOver from "./MessagePopOver";
+const MyMessages = () => {
+  return (
+    <>
+      <MessagesPopOver />
+    </>
+  );
+};
+
+export default MyMessages;
