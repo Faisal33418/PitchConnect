3fcec34c24ebfcd5981c801a8dd0792948f9623b
unknown <ahsanakmalbit99@gmail.com>
2025-04-28 10:28:28 +0500

    loading component

---
+++ src/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <h1 className="text-center text-4xl text-blue-500 animate-pulse">
+      Loading...
+    </h1>
+  );
+};
+
+export default loading;
